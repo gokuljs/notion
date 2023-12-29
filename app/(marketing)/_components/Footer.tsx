d9071b668bab6bf02b50e516c384b8ba52539619
gokuljs <gokuljs029@gmail.com>
2023-12-30 00:02:10 +0530

    privacy policy

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Logo from "./Logo";
+import { Button } from "@/components/ui/button";
 
 const Footer = () => {
   return (
     <div className="flex flex-items w-full p-6 bg-background z-50">
       <Logo />
+      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+        <Button variant={"ghost"} size={"sm"}>
+          Privacy Policy
+        </Button>
+        <Button variant={"ghost"} size={"sm"}>
+          Terms & condition
+        </Button>
+      </div>
     </div>
   );
 };
