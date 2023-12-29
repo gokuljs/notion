58e60aa51cce1b8c00c550b20aad3885f38f441b
gokuljs <gokuljs029@gmail.com>
2023-12-29 23:28:03 +0530

    home

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const MarketingPage = () => {
+  return <div className="min-h-full flex flex-col"></div>;
+};
+
+export default MarketingPage;
