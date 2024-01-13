8f7770339b620c1f7bb52042c554e4ced59107ce
gokuljs <gokuljs029@gmail.com>
2024-01-13 18:16:05 +0530

    editor

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Cover from '@/app/(main)/_components/Cover';
 import ToolBar from '@/app/(main)/_components/ToolBar';
+import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useQuery } from 'convex/react';
@@ -17,7 +18,19 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
         documentId: params.documentId
     });
     if (document === undefined) {
-        return <div>loading ....</div>;
+        return (
+            <div>
+                <Cover.Skeleton />
+                <div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
+                    <div className='space-y-4 pl-8 pt-4'>
+                        <Skeleton className='h-4 w-[50%]' />
+                        <Skeleton className='h-4 w-[80%]' />
+                        <Skeleton className='h-4 w-[40%]' />
+                        <Skeleton className='h-4 w-[60%]' />
+                    </div>
+                </div>
+            </div>
+        );
     }
     if (document === null) {
         return <div>Not found</div>;
--- app/(main)/_components/Cover.tsx
+++ app/(main)/_components/Cover.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useCoverImage } from '@/hooks/use-cover-Image';
@@ -74,3 +75,7 @@ const Cover = ({ url, preview }: CoverImageProps) => {
 };
 
 export default Cover;
+
+Cover.Skeleton = () => {
+    return <Skeleton className='w-full h-[12vh]' />;
+};
