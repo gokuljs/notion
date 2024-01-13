ab3e28f656006ad770e26ec2c8957fd93ae5ec0a
gokuljs <gokuljs029@gmail.com>
2024-01-13 18:06:35 +0530

    cover image

--- app/(main)/_components/Cover.tsx
+++ app/(main)/_components/Cover.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useCoverImage } from '@/hooks/use-cover-Image';
+import { useEdgeStore } from '@/lib/edgestore';
 import { cn } from '@/lib/utils';
 import { useMutation } from 'convex/react';
 import { ImageIcon, X } from 'lucide-react';
@@ -18,11 +19,17 @@ const Cover = ({ url, preview }: CoverImageProps) => {
     const params = useParams();
     const coverImage = useCoverImage();
     const remove = useMutation(api.documents.removeCoverImage);
+    const { edgestore } = useEdgeStore();
 
-    const removeImage = () => {
-        remove({
-            id: params.documentId as Id<'documents'>
-        });
+    const removeImage = async () => {
+        if (url) {
+            await edgestore.publicFiles.delete({
+                url: url
+            });
+            remove({
+                id: params.documentId as Id<'documents'>
+            });
+        }
     };
     return (
         <div
@@ -43,7 +50,7 @@ const Cover = ({ url, preview }: CoverImageProps) => {
             {!!url && !preview && (
                 <div className='opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2'>
                     <Button
-                        onClick={coverImage.onOpen}
+                        onClick={() => coverImage.onReplace(url)}
                         className='text-muted-foreground text-xs'
                         variant='outline'
                         size='sm'
